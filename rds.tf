2545d46960f92d3e257a755b25c65908aeec9b97
KaioAmaralB <kaio411@hotmail.com>
2024-03-15 23:36:30 -0300

    Trocando nome secrets

--- rds.tf
+++ rds.tf
@@ -66,7 +66,7 @@ resource "random_string" "password" {
 }
 
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name        = "db_credentials"
+  name        = "dbcredentials"
 }
 
 resource "aws_secretsmanager_secret_version" "db_credentials_version" {
